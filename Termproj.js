9fd6784b0544c64ecf492f4ed44f73fef5a89b98
skugger <154021691+skugger@users.noreply.github.com>
2024-03-23 12:07:21 -0400

    Add files via upload

---
+++ Termproj.js
@@ -0,0 +1,19 @@
+document.getElementById("home").addEventListener("mouseover",function(){
+    document.getElementById("description").innerHTML="<p><br><br>OK boomer!<br>Let's play a game!</p>";
+})
+
+document.getElementById("checkers").addEventListener("mouseover",function(){
+    document.getElementById("description").innerHTML="<p><br><br>This is a traditional checkers game.<br>Test your skill against the computer!</p>";
+})
+
+document.getElementById("snake").addEventListener("mouseover",function(){
+    document.getElementById("description").innerHTML="<p><br><br>Use the arrow keys to move around the screen.<br>Eat the bugs, and your snake grows longer!<br>Can you fill the entire screen?</p>";
+})
+
+document.getElementById("trivia").addEventListener("mouseover",function(){
+    document.getElementById("description").innerHTML="<p><br><br>This is a traditional trivia game.<br>Test your knowledge!</p>";
+})
+
+document.getElementById("readme").addEventListener("mouseover",function(){
+    document.getElementById("description").innerHTML="<p><br><br>game descriptions, file lists, copyright info,<br>and contact info goes here.</p>";
+})

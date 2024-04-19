c8b7dcb06b86a2847348179f3286dffee824603f
skugger <154021691+skugger@users.noreply.github.com>
2024-04-19 16:51:37 -0400

    Add files via upload
    milestone 2 uploaded

---
+++ Readme.txt
@@ -0,0 +1,16 @@
+
+
+	Drag & Drop game
+
+	Help Johnny and Jenny get ready for Open Mic Night!  Drag the images into place on stage, as you see fit.
+	All images obtained from Pixabay.
+
+
+
+	Slalom
+
+	This is a variation on the game that you learn to code when you go through the W3 Schools Canvas tutorial.
+	Rather than avoiding walls, use the arrow keys to avoid the oncoming objects.
+	The farther to the right you play, the faster the game goes, and the faster your score rises!
+
+
---
+++ js/Termproj.js
@@ -0,0 +1,19 @@
+document.getElementById("home").addEventListener("mouseover",function(){
+    document.getElementById("description").innerHTML="<p><br><br>OK boomer!<br>Let's play a game!</p>";
+})
+
+document.getElementById("dragndrop").addEventListener("mouseover",function(){
+    document.getElementById("description").innerHTML="<p><br><br>Johnny & Jenny are performing at Open Mic Night.<br><br>Click <a href='draganddrop.html'>here</a> to help them get ready for their gig!<br><br>Click <a href='readme.txt'>here</a> to see the Readme.txt File</p>";
+})
+
+document.getElementById("slalom").addEventListener("mouseover",function(){
+    document.getElementById("description").innerHTML="<p><br><br>Use the arrow keys to move around the screen,<br>and to stay alive!<br>Go faster to score points faster!<br><br>Click <a href='slalom.html'>here</a> to give it a try!<br><br>Click <a href='readme.txt'>here</a> to see the Readme.txt File</p>";
+})
+
+document.getElementById("trivia").addEventListener("mouseover",function(){
+    document.getElementById("description").innerHTML="<p><br><br>This is a traditional trivia game.<br>Test your knowledge!<br><br>Click <a href='readme.txt'>here</a> to see the Readme.txt File</p>";
+})
+
+document.getElementById("readme").addEventListener("mouseover",function(){
+    document.getElementById("description").innerHTML="<p><br><br>game descriptions, file lists, copyright info,<br>and contact info goes here.<br><br>Click <a href='readme.txt'>here</a> to see the Readme.txt File</p>";
+})
